93ae9eb6685337e7b4051995d742e1300259e440
ahad <ahad.j.65@gmail.com>
2025-11-18 12:05:00 +0330

    first

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := concealjni
-LOCAL_CFLAGS    := -fvisibility=hidden -Os -fdata-sections -ffunction-sections -fexceptions -std=gnu++1y
+LOCAL_CFLAGS    := -fvisibility=hidden -Os -fdata-sections -ffunction-sections -fexceptions -std=c++14
 LOCAL_SRC_FILES := CipherHybrid.cpp DecryptHybrid.cpp EncryptHybrid.cpp JavaArrays.cpp JKeyChain.cpp KeyChainFromJava.cpp MacDecoderHybrid.cpp MacEncoderHybrid.cpp OnLoad.cpp PBKDF2Hybrid.cpp
 LOCAL_LDLIBS    := -llog
 LOCAL_LDFLAGS   += -Wl,--gc-sections -Wl,--exclude-libs,ALL
